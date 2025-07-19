9f9625265b1f3b2ee2ef6059503454749bd2b470
Saman <dastgheib_saman@yahoo.com>
2025-07-19 10:53:23 +0200

    creating backend infrastructure

--- app/crud.py
+++ app/crud.py
@@ -14,31 +14,5 @@ def create_event(db: Session, event: schema.EventCreate):
     db.refresh(db_event)
     return db_event
 
-def get_teams(db: Session):
-    return db.query(models.Team).all()
 
-def get_team_by_id(db: Session, team_id: int):
-    return db.query(models.Team).filter(models.Team.id == team_id).first()
-
-def create_team(db: Session, team: schema.TeamCreate):
-    db_team = models.Team(**team.dict())
-    db.add(db_team)
-    db.commit()
-    db.refresh(db_team)
-    return db_team
-
-def update_team(db: Session, team_id: int, team: schema.TeamCreate):
-    db_team = db.query(models.Team).filter(models.Team.id == team_id).first()
-    if db_team:
-        db_team.name = team.name
-        db.commit()
-        db.refresh(db_team)
-    return db_team
-
-def delete_team(db: Session, team_id: int):
-    db_team = db.query(models.Team).filter(models.Team.id == team_id).first()
-    if db_team:
-        db.delete(db_team)
-        db.commit()
-    return db_team
 
